13bac1afd192d93671e90fa8a2b1b55c92028f1c
Martin Schorb <martin.schorb@embl.de>
2018-05-16 13:07:28 +0200

    added setup.py to make it easy to install emtools using pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+ # -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(name='emtools',
+      version='1.0',
+      py_modules=['emtools'],
+      description='Tools to interact with Serial EM enabling automated transmission electron microscopy.',
+      author='Martin Schorb',
+      author_email='schorb@embl.de',
+      ) 
