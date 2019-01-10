52c294e92f69a7813dd61f70b02e114edce0af08
Martin Schorb <martin.schorb@embl.de>
2019-01-10 15:50:45 +0100

    Upload New File

---
+++ tutorials/callpython.bat
@@ -0,0 +1,8 @@
+set root=C:\Users\emcf\Anaconda3
+
+call %root%\Scripts\activate.bat %root%
+call python %1 %2 %3 %4
+
+echo Execution ended, will close this prompt in 10 seconds...
+timeout /T 10
+exit
