21fe594fc0b06d957aed82c04f18d3940b266d2d
Martin Schorb <martin.schorb@embl.de>
2019-01-17 15:36:21 +0100

    Update knimepython.bat

--- tutorials/knimepython.bat
+++ tutorials/knimepython.bat
@@ -1,4 +1,4 @@
-@REM Adapt the directory in the PATH to your system    
+@REM Adapt the directory in the PATH to your system's anaconda installation    
   @SET PATH=C:\User\YOURUSER\Anaconda3\Scripts;%PATH%  
   @CALL activate base || ECHO Activating anaconda failed  
   @python %*
