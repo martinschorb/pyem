7bdadd0b26ad6157019c787cafb2dec81458aa31
Martin Schorb <schorb@embl.de>
2019-05-28 14:59:16 +0200

    mrcfile version issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='py-EM',
-      version='20190502',
+      version='20190528',
       py_modules=['pyEM'],
       description='Tools to interact with Serial EM enabling automated transmission electron microscopy.',
       long_description=long_description,
@@ -18,7 +18,7 @@ setup(name='py-EM',
       'numpy',
       'scipy',
       #'re',
-      'mrcfile',
+      'mrcfile==1.1.1',
       #'time',
       #'operator',      
       ],
