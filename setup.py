76854f0064c6b4d1a294096659c8c5acf843e975
Martin Schorb <schorb@embl.de>
2019-07-22 11:42:06 +0200

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='py-EM',
-      version='20190710',
+      version='20190722',
       py_modules=['pyEM'],
       description='Tools to interact with Serial EM enabling automated transmission electron microscopy.',
       long_description=long_description,
