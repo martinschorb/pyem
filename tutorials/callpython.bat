ff262a67cb5ca4e14ca49582810119ba3458187e
Martin Schorb <schorb@embl.de>
2019-07-24 15:15:55 +0200

    fix encoding for windows, set output to quiet

--- tutorials/callpython.bat
+++ tutorials/callpython.bat
@@ -1,8 +1,15 @@
+@echo off
+
 set root=C:\Users\YOURUSER\Anaconda3
 
+
 call %root%\Scripts\activate.bat %root%
+
 call python %1 %2 %3 %4
 
+
 echo Execution ended, will close this prompt in 10 seconds...
+
 timeout /T 10
+
 exit
