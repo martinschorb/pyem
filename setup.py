791162cb25d1c464c279302b16c563755a34dc66
Martin Schorb <schorb@embl.de>
2019-08-16 16:38:45 +0200

    commit hook test for setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='py-EM',
-      version='20190816',
+      version='test2',
       py_modules=['pyEM'],
       description='Tools to interact with Serial EM enabling automated transmission electron microscopy.',
       long_description=long_description,
