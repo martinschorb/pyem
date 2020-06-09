93e54464fb5c4ed0e9b9323cf8b0ded19bfb429e
Martin Schorb <schorb@embl.de>
2020-06-09 16:43:17 +0200

    update version to 20200609

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='py-EM',
-      version='20200603',
+      version='20200609',
       py_modules=['pyEM'],
       description='Tools to interact with Serial EM enabling automated transmission electron microscopy.',
       long_description=long_description,
