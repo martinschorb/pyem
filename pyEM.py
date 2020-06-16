fbae18ec65bde4b36ff035ade644d63513e97f29
Martin Schorb <martin.schorb@embl.de>
2020-06-16 17:32:05 +0200

    bugfix mergemap: offset for single-tile mrc maps

--- pyEM.py
+++ pyEM.py
@@ -514,9 +514,10 @@ def mergemap(mapitem,crop=False,black=False,blendmont=True):
   m['frames'] = list(map(int,mapitem['MapFramesXY']))
   montage_tiles = numpy.prod(m['frames'])
   
+  tileidx_offset = 0
+  
   if mapfile.find('.st')<0 and mapfile.find('.map')<0 and mapfile.find('.mrc')<0:
     #not an mrc file
-    tileidx_offset = 0
    
     mergeheader = {}
     mergeheader['stacksize'] = 1
