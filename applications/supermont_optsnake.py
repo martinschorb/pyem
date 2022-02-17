03568b74c2ca0b4dfb97c4e2bc1e83a78c06c387
Martin Schorb <martin.schorb@embl.de>
2022-02-17 17:26:27 +0100

    snakes

--- applications/supermont_optsnake.py
+++ applications/supermont_optsnake.py
@@ -2,6 +2,7 @@
 # import the py-EM module and make its functions available
 import pyEM as em
 import numpy as np
+from scipy.cluster.vq import kmeans, whiten
 
 # parse command line parameters
 
@@ -24,78 +25,131 @@ def neighbours(inarr,pt,dist=1):
     p0 = np.repeat(pt[0]+i_range,len(i_range))
     p1 = np.tile(pt[1]+i_range,len(i_range))
     allnbrs = np.stack([p0,p1],axis=-1)
-
     eu_dist = np.linalg.norm(allnbrs-pt,axis=1)
     circ = allnbrs[np.argwhere(np.all(np.stack([eu_dist<=dist,eu_dist>(dist-1)],axis=1),axis=1))].squeeze()
-
     out=[]
     for nb in circ:
         if any(np.all(nb-inarr==[0,0],axis=1)): out.append(nb)
-
     return np.array(out,dtype=int)
 
 
-def step(inarr,pt,maxdist=6):
+def step(inarr,pt,maxdist=6,nb=True):
     pt_index = np.argwhere(np.all(inarr-pt==[0,0],axis=1))
     outarr = np.delete(inarr,pt_index,0)
     nbs = []
     dist = 0
-
-    while len(nbs) < 1 and dist < maxdist:
-        dist += 1
-        nbs = neighbours(outarr,pt,dist=dist)
-
-    if len(nbs)>0:
-        outpt = nbs[int(np.random.random() * len(nbs))]
+    outpt = [0, 0]
+    if nb:
+        while len(nbs) < 1 and dist < maxdist:
+            dist += 1
+            nbs = neighbours(outarr,pt,dist=dist)
+        if len(nbs)==0:
+            outarr = []
+        else:
+            outpt = nbs[int(np.random.random() * len(nbs))]
     else:
-        outarr=[]
-        outpt=[0,0]
+        if len(outarr)>0:
+            outpt = outarr[int(np.random.random() * len(outarr))]
 
     return outarr, outpt, np.linalg.norm(outpt-pt)
 
-def tour(inarr,start,maxdist=6):
+
+def tour(inarr,start,maxdist=6,nb=True):
     pt = np.copy(start)
     outarr = np.copy(inarr)
     dist=0
     route = [start]
-
     for leg,tpt in enumerate(inarr):
-        outarr,pt,stepdist = step(outarr,pt,maxdist=maxdist)
-        if len(outarr) < 1 and leg < len(inarr-1):
-            return None,[]
-        route.append(pt)
-        dist+=stepdist
+        outarr,pt,stepdist = step(outarr,pt,maxdist=maxdist,nb=nb)
 
+        if leg < len(inarr)-1:
+            if len(outarr) < 1:
+                return None, []
+            route.append(pt)
+            dist+=stepdist
     return dist,np.array(route)
 
-def randomtour(inarr,numtrials = 20):
 
+def randomtour(inarr,start=None,maxdist=4,numtrials = 10,subregions='',nb=True):
     dists = []
     routes = []
     pidx = -1
-    start = inarr[0]
+    if start is  None:
+        start = inarr[0]
 
     for i in range(numtrials):
         pidx0 = int(i/numtrials*100)
         if pidx0 != pidx:
-            print('Searching... - '+str(pidx0)+'% completed.')
+            print('Searching '+subregions+'... - '+str(pidx0)+'% completed.')
             pidx=pidx0
-
         d = None
-
         while d is None:
-            d, route = tour(inarr, start,maxdist=10)
-
+            d, route = tour(inarr, start,maxdist=maxdist,nb=nb)
         routes.append(route)
         dists.append(d)
-
     return dists,routes
+
+
+
+def splitcoos(inarr,size=50):
+    n,rem=np.divmod(len(inarr),size)
+    n+=1
+
+    whitened = whiten(inarr)
+    clusterpts,distortion = kmeans(whitened, n)
+
+    dists, routes = randomtour(clusterpts,nb=False,numtrials=100,subregions='region centers')
+
+    clusterpts = routes[np.argmin(dists)]
+
+    cp_dists = []
+    for cp in clusterpts:
+        cp_dists.append(list(np.linalg.norm(whitened - cp,axis=1)))
+
+    cluster=np.argmin(np.array(cp_dists),axis=0)
+
+    return cluster
+
+
+def connectregions(inarr,cluster):
+    regions =[]
+
+    for region in np.unique(cluster):
+        regions.append(inarr[cluster==region])
+
+    fullroute = []
+
+    r_idx = 0
+    nextpoint_ix = np.argmin(np.linalg.norm(regions[r_idx + 1] - np.mean(regions[r_idx], axis=0), axis=1))
+    startpoint = regions[r_idx + 1][nextpoint_ix]
+    thisregion = np.vstack((regions[r_idx], startpoint))
+    # regions[r_idx + 1] = np.delete(regions[r_idx + 1], nextpoint_ix,axis=0)
+    dists, routes = randomtour(thisregion, start=startpoint, maxdist=3, numtrials=20,subregions='subregion 1/'+str(len(regions)))
+
+    for rt_ix,rt in enumerate(routes):
+        routes[rt_ix] = np.flipud(rt)
+
+
+    fullroute.extend(routes[np.argmin(dists)][:-1])
+
+    for r_idx in range(len(regions)-1):
+        dists, routes = randomtour(regions[r_idx+1], start=startpoint, maxdist=3, numtrials=200,subregions='subregion '+str(r_idx+2)+'/'+str(len(regions)))
+        stpts=[]
+        if r_idx < len(regions) - 2:
+            for rt_idx,route in enumerate(routes):
+                next_dist = np.linalg.norm(regions[r_idx + 2] - route[-1], axis=1)
+                nextpoint_ix = np.argmin(next_dist)
+                dists[rt_idx] += np.min(next_dist)
+                stpts.append(regions[r_idx + 2][nextpoint_ix])
+
+            startpoint = stpts[np.argmin(dists)]
+        fullroute.extend(routes[np.argmin(dists)])
+
+    return np.array(fullroute)
 
 # load the navigator file
 navlines = em.loadtext(navfile)
 allitems = em.fullnav(navlines)
-
-
 
 sm_items = list(filter(lambda item:item.get('SuperMontXY'),allitems))
 
@@ -113,26 +167,32 @@ smitems = np.array(sm_items)
 out_sm = []
 
 for sm in np.unique(supermont):
-
     thiscoos = smcoos[np.where(np.array(supermont)==sm)]
     thisitems = smitems[np.where(np.array(supermont)==sm)]
-    sm_range = np.max(thiscoos, 0) - np.min(thiscoos, 0)
 
+    cluster = splitcoos(thiscoos)
+    fullroute = connectregions(thiscoos, cluster)
 
+    route_idx=[]
+    route_polyX = []
+    route_polyY = []
 
+    for pt in fullroute:
+        current = int(np.argwhere(np.all(thiscoos-pt==[0,0],axis=1)))
+        route_idx.append(current)
 
-    snakedim = np.argmin(sm_range)
+        out_sm.append(sm_items[current])
+        route_polyX.append(sm_items[current]['StageXYZ'][0])
+        route_polyY.append(sm_items[current]['StageXYZ'][1])
 
-    templist = []
+    poly = em.pointitem('SM_'+str(sm)+'_start')
+    poly['Type'] = '1' #polygon
+    poly['StageXYZ'] = sm_items[route_idx[0]]['StageXYZ']
+    poly['NumPts'] = [len(fullroute)]
+    poly['PtsX'] = route_polyX
+    poly['PtsY'] = route_polyY
 
-    for idx,leg in enumerate(thiscoos):
-        if np.divmod(leg[1],2)[1]>0:
-            templist.reverse()
-            out_sm.extend(templist)
-            templist = []
-            out_sm.append(thisitems[idx])
-        else:
-            templist.append(thisitems[idx])
+    out_sm.append(poly)
 
 non_sm = [x for x in allitems if x not in sm_items]
 #newnav = non_acq
@@ -149,8 +209,8 @@ outnav = list()
 # fill the new file   
 outnav.extend(non_sm)
 outnav.extend(out_sm)
-    
+
+print('----------------------------------------\n Done.\n Writing output Supermontage as '+newnavf
     
 em.write_navfile(newnavf,outnav,xml=False)
 
-    
--- applications/supermont_snake.py
+++ applications/supermont_snake.py
@@ -41,24 +41,47 @@ smitems = np.array(sm_items)
 out_sm = []
 
 for sm in np.unique(supermont):
-
+    out_sm0 = []
     thiscoos = smcoos[np.where(np.array(supermont)==sm)]
     thisitems = smitems[np.where(np.array(supermont)==sm)]
     sm_range = np.max(thiscoos, 0) - np.min(thiscoos, 0)
 
+    if sm_range[1]<sm_range[0]:
+        sortidx0 = np.argsort(thiscoos[:,1])
+        sortidx1 = np.argsort(thiscoos[:,0],kind='mergesort')
+        thiscoos = np.fliplr(thiscoos[sortidx1])
+        thisitems = thisitems[sortidx1]
 
-    snakedim = np.argmin(sm_range)
-
-    templist = []
+    templist=[]
 
     for idx,leg in enumerate(thiscoos):
+        print('checking line '+str(idx+1)+'...')
         if np.divmod(leg[1],2)[1]>0:
             templist.reverse()
-            out_sm.extend(templist)
+            out_sm0.extend(templist)
             templist = []
-            out_sm.append(thisitems[idx])
+            out_sm0.append(thisitems[idx])
         else:
             templist.append(thisitems[idx])
+
+    route_polyX = []
+    route_polyY = []
+
+    for item in out_sm0:
+        current = sm_items.index(next(filter(lambda itm: itm['# Item'] == item['# Item'], sm_items)))
+        route_polyX.append(sm_items[current]['StageXYZ'][0])
+        route_polyY.append(sm_items[current]['StageXYZ'][1])
+
+    poly = em.pointitem('SM_'+str(sm)+'_start')
+    poly['Type'] = '1' #polygon
+    poly['StageXYZ'] = thisitems[0]['StageXYZ']
+    poly['NumPts'] = [str(len(out_sm0))]
+    poly['PtsX'] = route_polyX
+    poly['PtsY'] = route_polyY
+
+    out_sm.extend(out_sm0)
+
+    out_sm.append(poly)
 
 non_sm = [x for x in allitems if x not in sm_items]
 #newnav = non_acq
@@ -72,11 +95,10 @@ outnav = list()
 #nnf.write("%s\n" % navlines[0])
 #nnf.write("%s\n" % navlines[1])
 
-# fill the new file   
+# fill the new file
 outnav.extend(non_sm)
 outnav.extend(out_sm)
-    
-    
-em.write_navfile(newnavf,outnav,xml=False)
 
-    
+print('----------------------------------------\n Done.\n Writing output Supermontage as '+newnavf+'.')
+
+em.write_navfile(newnavf,outnav,xml=False)
