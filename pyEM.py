ede64ccaa3fad79c4b0ce9b65b71fcbbf186eecd
Martin Schorb <martin.schorb@embl.de>
2022-02-24 16:25:28 +0100

    change adoc item label to full item instead of search string

--- pyEM.py
+++ pyEM.py
@@ -177,7 +177,7 @@ def adoc_items(lines1,search,header=False):
                 item = lines[itemstartline:itemstartline+itemendline]
                 
                 itemdict = parse_adoc(item)
-                itemdict['# '+item]=lines[itemstartline-1][lines[itemstartline-1].find(' = ') + 3:-1]
+                itemdict['# '+searchstr]=lines[itemstartline-1][lines[itemstartline-1].find(' = ') + 3:-1]
             result.append(itemdict)
 
     return result
