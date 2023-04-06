ce04f5db44f72f30e6e4ebef54d1748e0c988165
Martin Schorb <martin.schorb@embl.de>
2023-04-06 11:22:17 +0200

    add expected values

--- test/test_data.py
+++ test/test_data.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 example_mapz = os.path.join('test_files', 'maps.tgz')
 example_map = os.path.abspath(os.path.join('test_files', 'MMM_01.mrc'))
@@ -8,3 +9,6 @@ if not os.path.exists(example_map):
         os.system('tar xvfz ' + example_mapz + ' -C ' + 'test_files')
     except OSError as e:
         pass
+
+with open('test_files/test_expectedvalues.json') as f:
+    expected = json.load(f)
